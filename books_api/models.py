378c8d263194725013769881c85d8d8894158a64
wnadobnik <w.nadobnik@gmail.com>
2021-10-12 00:19:03 +0200

    v. 2.0

--- books_api/models.py
+++ books_api/models.py
@@ -12,7 +12,15 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+class BookManager(models.Manager):
+    def safe_get(self, *args, **kwargs):
+        try:
+            return self.get(*args, **kwargs)
+        except Book.DoesNotExist:
+            return False
+
 class Book(models.Model):
+    objects = BookManager()
     title = models.CharField(max_length=400)
     authors = models.ManyToManyField(Author)
     categories = models.ManyToManyField(Category)
@@ -23,17 +31,25 @@ class Book(models.Model):
 
     def add_authors(self, new_authors):
         for author in new_authors:
-            if not self.authors.filter(name=author):
-                if not Author.objects.filter(name=author):
-                    self.authors.create(name=author)
-                else:
-                    self.authors.add(Author.objects.get(name=author))
+            try:
+                a_obj = Author.objects.get(name=author)
+            except:
+                a_obj = Author.objects.create(name=author)
+            self.authors.add(a_obj)
+
+    def compare_authors(self, authors):
+        current = self.authors.values_list('name', flat=True)
+        for author in authors:
+            if author not in current:
+                return False
+        return True
 
     def update_categories(self, new_categories):
         if new_categories:
             self.categories.clear()
             for category in new_categories:
-                if not Category.objects.filter(title=category):
-                    self.categories.create(title=category)
-                else:
-                    self.categories.add(Category.objects.get(title=category))
+                try:
+                    c_obj = Category.objects.get(title=category)
+                except:
+                    c_obj = Category.objects.create(title=category)
+                self.categories.add(c_obj)
--- books_api/views.py
+++ books_api/views.py
@@ -1,81 +1,67 @@
 from books_api.serializers import BookSerializer
 from books_api.models import Book
-from rest_framework.views import APIView
 import requests
+from django_filters import rest_framework as filters
 from rest_framework.response import Response
+from rest_framework import generics
 
+class BookFilter(filters.FilterSet):
+    author = filters.CharFilter(field_name="authors__name", lookup_expr='contains')
+    sort = filters.OrderingFilter(
+        fields=(
+            ('published_date', 'published_date')
+        )
+    )
 
-class BookViewSet(APIView):
+    class Meta:
+        model = Book
+        fields = ['published_date']
+
+class BookViewSet(generics.ListAPIView):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-
-    def get(self, request, format=None):
-        result = Book.objects.all()
-        published_date = request.query_params.get('published_date')
-        authors = request.query_params.getlist('author', '')
-        sort = request.query_params.get('sort')
-        if published_date:
-            if isinstance(published_date, int) and len(published_date) <= 4:
-                result = result.filter(published_date=published_date)
-        if authors:
-            result = result.filter(authors__name=authors)
-        if sort == '-published_date' or sort == "published_date":
-            result = result.order_by(sort)
-        serializer = BookSerializer(result.distinct(), many=True)
-        return Response(serializer.data)
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = BookFilter
 
     def post(self, request):
-        books_standardized = []
         q = request.query_params.get('q')
         if q:
             url = f'https://www.googleapis.com/books/v1/volumes?q={q}'
             r = requests.get(url)
             books = r.json()['items']
             for book in books:
-                books_standardized.append({
-                    'title': book['volumeInfo']['title'],
-                    'authors': book['volumeInfo']['authors'],
-                    'categories': book['volumeInfo'].get('categories'),
-                    'published_date': book['volumeInfo']['publishedDate'][0:4],
-                    'average_rating': book['volumeInfo'].get('averageRating'),
-                    'ratings_count': book['volumeInfo'].get('ratingsCount'),
-                    'thumbnail': book['volumeInfo']['imageLinks'].get('thumbnail') if book['volumeInfo'].get(
-                        'imageLinks') else None})
-        else:
-            books_standardized.append({
-                'title': request.query_params.get('title'),
-                'authors': request.query_params.get('authors').split(','),
-                'categories': request.query_params.get('categories').split(','),
-                'published_date': request.query_params.get('published_date'),
-                'average_rating': request.query_params.get('average_rating'),
-                'ratings_count': request.query_params.get('ratings_count'),
-                'thumbnail': request.query_params.get('thumbnail')})
-
-        for book_standardized in books_standardized:
-            book_operate = Book.objects.filter(title=book_standardized['title'],
-                                               authors__name=book_standardized['authors'],
-                                               published_date=book_standardized['published_date'])
-            if book_operate:
-                book_operate.save(average_rating=book_standardized['average_rating'],
-                                  ratings_count=book_standardized['ratings_count'],
-                                  thumbnail=book_standardized['thumbnail'])
-            else:
-                book_operate = Book(title=book_standardized['title'],
-                                    published_date=book_standardized['published_date'],
-                                    average_rating=book_standardized['average_rating'],
-                                    ratings_count=book_standardized['ratings_count'],
-                                    thumbnail=book_standardized['thumbnail'])
-                book_operate.save()
-                book_operate.add_authors(book_standardized['authors'])
-            book_operate.update_categories(book_standardized['categories'])
+                title = book['volumeInfo']['title']
+                authors = book['volumeInfo'].get('authors', [])
+                categories = book['volumeInfo'].get('categories')
+                published_date = int(book['volumeInfo']['publishedDate'][:4]) if book['volumeInfo'].get('publishedDate',False) else False
+                average_rating = book['volumeInfo'].get('averageRating', None)
+                ratings_count = book['volumeInfo'].get('ratingsCount', None)
+                thumbnail = book['volumeInfo']['imageLinks'].get('thumbnail', None) if book['volumeInfo'].get('imageLinks', False) else False
+                get_book = Book.objects.safe_get(
+                        title=title,
+                        published_date=published_date)
+                if get_book and get_book.compare_authors(authors):
+                    get_book.average_rating = average_rating
+                    get_book.ratings_count = ratings_count
+                    get_book.thumbnail = thumbnail
+                    get_book.save()
+                else:
+                    new_book = Book(title=title,
+                        published_date=published_date,
+                        average_rating=average_rating,
+                        ratings_count=ratings_count,
+                        thumbnail=thumbnail)
+                    new_book.save()
+                    new_book.add_authors(authors)
+                    new_book.update_categories(categories)
         library = Book.objects.all().distinct()
         serializer = BookSerializer(library, many=True)
         return Response(serializer.data)
 
 
-class BookView(APIView):
+class BookView(generics.ListAPIView):
     serializer_class = BookSerializer
 
     def get_queryset(self):
         id = self.kwargs['id']
-        return Book.objects.get(id=id)
+        return [Book.objects.get(id=id)]
